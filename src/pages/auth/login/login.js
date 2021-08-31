59512b3e1dc4d84e212720f319842fe02a68a5b3
zlade30 <zal.dev.30@gmail.com>
2021-09-01 07:07:09 +0800

    Create custom checkbox

--- src/pages/auth/login/login.js
+++ src/pages/auth/login/login.js
@@ -7,21 +7,37 @@ const Login = () => {
   return (
     <div className="auth-container">
       <div className="auth-form">
-        <div className="logo">
-          <img className="logo-img" src="assets/icons/dog.png" />
-          Dog Saver
+        <div>
+          <div className="logo">
+            <img className="logo-img" src="assets/icons/dog.png" />
+            Dog Saver
+          </div>
+          <TextField
+            width={320}
+            placeholder="Email or Phone"
+            errorLabel="Please input your email or phone!"
+          />
+          <PasswordField
+            width={320}
+            placeholder="Password"
+            errorLabel="Please input your password!"
+          />
+          <div className="checkbox-container form-item justify-between">
+            <label className="checkbox">
+              Remember Me
+              <input type="checkbox" />
+              <span className="checkmark"></span>
+            </label>
+            <label className="forgot-password">Forgot Password?</label>
+          </div>
+          <Button width={320} value="Sign In" />
         </div>
-        <TextField
-          width={320}
-          placeholder="Email or Phone"
-          errorLabel="Please input your email or phone!"
-        />
-        <PasswordField
-          width={320}
-          placeholder="Password"
-          errorLabel="Please input your password!"
-        />
-        <Button width={320} value="Login" />
+        <div>
+          <span>
+            <label>Do not have an account yet?</label>
+            <label className="register">Sign Up</label>
+          </span>
+        </div>
       </div>
       <div className="auth-content"></div>
     </div>
