f4811d2b5aa345055eb1ab4db7c78feaaf89cb54
zlade30 <zal.dev.30@gmail.com>
2021-12-19 18:19:03 +0800

    Change announcement bg

--- src/components/announcement-message/AnnouncementMessage.js
+++ src/components/announcement-message/AnnouncementMessage.js
@@ -23,7 +23,8 @@ const AnnouncementMessage = ({ item, onArchive, onUpdate, role, onRestore }) => 
   )
 
   return (
-    <div className="announcement-message mb-10" style={{ paddingTop: 0, position: 'relative', overflow: 'hidden' }}>
+    <div className="announcement-message mb-10" style={{ paddingTop: 0, position: 'relative', overflow: 'hidden', backgroundColor: '#a1d3da' }}>
+      <img src="assets/icons/bg.png" style={{ position: 'absolute', left: 600, width: 150, height: 100, bottom: 0, zIndex: 0 }} />
       <div className="ancmnt-msg-header" style={{ zIndex: 2 }}>
         <div className="ancmnt-msg-header-icon">
           <div className="flex items-center">
@@ -57,7 +58,7 @@ const AnnouncementMessage = ({ item, onArchive, onUpdate, role, onRestore }) => 
             <div />
           )}
         </div>
-        <Divider width="95%" />
+        <Divider width="95%" color="black" />
       </div>
       <div className="ancmnt-msg-content">
         <label style={{ zIndex: 2 }}>{item?.content}</label>
